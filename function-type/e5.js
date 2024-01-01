9c35f3ebcdb142ea2b11b30633432c9cb6f7a570
prabinnnn <prabinbhandari120@gmail.com>
2024-01-01 16:54:48 +0545

    add q4

---
+++ function-type/e5.js
@@ -0,0 +1,8 @@
+const reverse = (num) => {
+  let data = string(num);
+  let lengthofdata = datalength;
+  for (let i = data.legth; i >= 0; i--) {
+    console.log(data[i]);
+  }
+};
+reverser(32243);
---
+++ function-type/fun11.js
@@ -0,0 +1,9 @@
+const reverse = (num) => {
+  let data = string(num);
+  let lengthofdata = datalength;
+  let temp = "";
+  for (let i = data.legth - 1; i >= 0; i--) {
+    console.log(data[i]);
+  }
+};
+reverser(32243);
