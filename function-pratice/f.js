4717cf7fcec16400c20a9f737a301e6955e43ae1
prabinnnn <prabinbhandari120@gmail.com>
2024-01-01 22:31:59 +0545

    add f1

--- function-pratice/f.js
+++ function-pratice/f.js
@@ -2,10 +2,11 @@ function vowelinsting(num) {
   let arr = "aeiouAEIOU";
   let vcount = 0;
   for (let i = 0; i <= num.length; i++) {
-    if (arr === num[i]) {
-      console.log(arr);
+    if (arr == num[i]) {
+      vcount == arr[i];
       vcount++;
     }
   }
+  return vcount;
 }
-console.log(vowelinsting("ihateuu"));
+console.log(vowelinsting("ihateuuol"));
