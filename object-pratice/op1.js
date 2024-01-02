5500cd141ffb661f2dcc444d1c1fe544d531d68a
prabinnnn <prabinbhandari120@gmail.com>
2024-01-02 21:19:19 +0545

    add op3

--- object-pratice/op1.js
+++ object-pratice/op1.js
@@ -5,8 +5,12 @@ let property = {
   discount1: "7%",
   mydis: function () {
     if (this.price > 100) {
-      return this.add(discount + price);
+      let discountedPrice =
+        this.price - (parseFloat(this.discount) / 100) * this.price;
+      return discountedPrice;
     }
-    return this.add(discount1 + price);
+    let discountedPrice1 =
+      this.price - (parseFloat(this.discount1) / 100) * this.price;
+    return discountedPrice1;
   },
 };
