6941c23633d10242293ef45dd14b8cfac8fe0e57
prabinnnn <prabinbhandari120@gmail.com>
2024-01-02 16:56:59 +0545

    add o1

--- function-pratice/f.js
+++ function-pratice/f.js
@@ -2,8 +2,8 @@ function vowelinsting(num) {
   let arr = "aeiouAEIOU";
   let vcount = 0;
   for (let i = 0; i < num.length; i++) {
-    if (arr == num[i]) {
-      vcount == arr[i];
+    if (arr === num[i]) {
+      vcount === arr[i];
       vcount++;
     }
   }
