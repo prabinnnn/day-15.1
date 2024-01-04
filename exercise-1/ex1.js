0acfe731d71f8ab5bd07f208d4d5d4ca9c239491
prabinnnn <prabinbhandari120@gmail.com>
2024-01-04 16:41:42 +0545

    add ex1

--- exercise-1/ex1.js
+++ exercise-1/ex1.js
@@ -1,7 +1,11 @@
-let newArr = [{ plaindrom: 121 }, { palindrom1: 10 }, { palindrome2: 123 }];
+let newArr = [
+  { plaindrom: String(121) },
+  { palindrom1: String(10) },
+  { palindrome2: String(123) },
+];
 let convertoplaindrome = function (newArr) {
   for (let i = 0; i < newArr.length; i++) {
-    let value = newArr[i][Object.keys(newArr[i])].toString();
+    let value = newArr[i][Object.keys(newArr[i])];
     let convert = value.split("").reverse().join("");
     if (convert === newArr) {
       return true;
