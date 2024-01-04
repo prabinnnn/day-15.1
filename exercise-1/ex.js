074e05a423823318c22f78f6a8044db1f5833d54
prabinnnn <prabinbhandari120@gmail.com>
2024-01-04 16:16:47 +0545

    add ex

--- exercise-1/ex.js
+++ exercise-1/ex.js
@@ -2,7 +2,8 @@ let convertTheString = function (arr) {
   let newArr = arr.split(" ");
   let minArr = [];
   for (let i = 0; i < newArr.length; i++) {
-    minArr.push(newArr[i].charAt(0).toUpperCase() + newArr[i].slice(1));
+   let con= newArr[i].charAt(0).toUpperCase() + newArr[i].slice(1);
+   let new=con.push();
   }
   return minArr.join(" ");
 };
--- exercise-1/ex1.js
+++ exercise-1/ex1.js
@@ -1,8 +1,7 @@
-let newArr = [{ plaindrom: 121 }, { palindrom: 10 }, { palindrome: 123 }];
-let convertoplaindrome = function (newarr) {
-  let arr = newarr;
-  for (let i = 0; i < arr.length; i++) {
-    let value = newArr[i][Object.keys(newArr[i][0])].toString();
+let newArr = [{ plaindrom: 121 }, { palindrom1: 10 }, { palindrome2: 123 }];
+let convertoplaindrome = function (newArr) {
+  for (let i = 0; i < newArr.length; i++) {
+    let value = newArr[i][Object.keys(newArr[i])].toString();
     let convert = value.split("").reverse().join("");
     if (convert === newArr) {
       return true;
