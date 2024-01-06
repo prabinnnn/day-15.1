3e66f1a2cb294ca27e78d90e4d9a3c61602a82d0
prabinnnn <prabinbhandari120@gmail.com>
2024-01-06 20:50:55 +0545

    add index

--- new/index.js
+++ new/index.js
@@ -2,3 +2,9 @@ function today() {
   return new Date();
 }
 document.getElementById("dateid").innerHTML = today();
+
+function getformdata() {
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  console.log(fnmae, lname);
+}
